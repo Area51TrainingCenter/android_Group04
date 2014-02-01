b0a2fe669a8dee4b57ab0c81c3cb558a02b58447
Segundo Acosta Delgado <segu19@gmail.com>
2014-01-31 21:01:27 -0500

    Clase05

--- module_1/clase_05/Clase05/src/com/area51/clase05/MainActivity.java
+++ module_1/clase_05/Clase05/src/com/area51/clase05/MainActivity.java
@@ -1,22 +1,109 @@
 package com.area51.clase05;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import com.area51.adapters.SectionFragmentPageAdapter;
 
-public class MainActivity extends Activity {
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+
+
+public class MainActivity extends FragmentActivity implements
+ 	ActionBar.TabListener
+{
+	
+	ActionBar mActionBar;
+	SectionFragmentPageAdapter mSectionFragmentPageAdapter;
+	ViewPager mViewPager;
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Creamos el ActionBar de navegación
+		
+		mActionBar = getActionBar();
+		mActionBar.setNavigationMode( ActionBar.NAVIGATION_MODE_TABS );
+		
+		//Creamos el manejador de los Fragments (FragmentPAgeAdapter)
+		
+		mSectionFragmentPageAdapter = 
+				new SectionFragmentPageAdapter(
+						getSupportFragmentManager() );
+		
+		//Creamos el panel donde se mostrarán los Fragments(ViewPager)
+		
+		mViewPager = (ViewPager)findViewById(R.id.viewPager);
+		//Esta linea es la poderosa que hace la magia
+		mViewPager.setAdapter(mSectionFragmentPageAdapter);
+		
+
+		//Ahora haremos que el mActionBar sea padre soltero :D
+		for (int i = 0; i < mSectionFragmentPageAdapter.getCount()  ; i++) {
+			
+			mActionBar.addTab( mActionBar.newTab()
+					.setText( mSectionFragmentPageAdapter.getTitle(i))
+					.setTabListener(this));			
+		}
+		
+		
+		//Ahora hacemos que el ViewPager y el ActionBar sean amigos :D
+		mViewPager.setOnPageChangeListener( new OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int position) {				
+				//Este codigo hace la magia
+				mActionBar.setSelectedNavigationItem(position);
+			}
+			
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		
+		
+		
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
 	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		//Codigo que hace la magia
+		mViewPager.setCurrentItem( tab.getPosition() );
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
+	
+	
+	
 
 }
--- module_1/clase_05/Clase05/src/com/area51/fragments/SectionFragment.java
+++ module_1/clase_05/Clase05/src/com/area51/fragments/SectionFragment.java
@@ -1,11 +1,38 @@
 package com.area51.fragments;
 
+
+import com.area51.clase05.R;
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SectionFragment extends Fragment {
 
+	public final static String FRAGMENT_POSITION = "FRAGMENT_POSITION";
+		
 	public SectionFragment() {
-		// TODO Auto-generated constructor stub
+		
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View vistaFragment = inflater.inflate(
+				R.layout.fragment, null);
+		
+		TextView textoFragment = (TextView)vistaFragment
+				.findViewById( R.id.textoFragment );
+		
+		textoFragment.setText("Fragmento"
+							+ getArguments().getInt(FRAGMENT_POSITION) );
+		
+		
+		return vistaFragment;
 	}
 
 }

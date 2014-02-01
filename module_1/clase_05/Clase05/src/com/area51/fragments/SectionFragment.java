a6c9d6c6387f1dcd015f077a5fb497ad30b2528d
Segundo Acosta Delgado <segu19@gmail.com>
2014-01-31 19:36:14 -0500

    avance clase05

---
+++ module_1/clase_05/Clase05/src/com/area51/fragments/SectionFragment.java
@@ -0,0 +1,11 @@
+package com.area51.fragments;
+
+import android.support.v4.app.Fragment;
+
+public class SectionFragment extends Fragment {
+
+	public SectionFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

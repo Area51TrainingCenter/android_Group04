23631fd2979db66050742076b26fb74e9c749a2d
Segundo Acosta Delgado <segu19@gmail.com>
2014-02-05 19:57:35 -0500

    Avance

--- module_1/clase_07/LogueoEstatico/src/com/area51/logueoestatico/MainActivity.java
+++ module_1/clase_07/LogueoEstatico/src/com/area51/logueoestatico/MainActivity.java
@@ -1,16 +1,41 @@
 package com.area51.logueoestatico;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
 
+	EditText txtUsuario;
+	EditText txtClave;
+	Button   btnIngreso;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		txtUsuario = (EditText)findViewById(R.id.txtUsuario);
+		txtClave = (EditText)findViewById(R.id.txtClave);
+		btnIngreso = (Button)findViewById(R.id.btnIngreso);
+		
+		btnIngreso.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				
+			}
+		});
+		
+		
+		
+		
 	}
-
 
 	
 	
